63f2aa02c177fc0194f044430bfe727a629c8a4b
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-02-28 10:42:56 +1100

    Update LWC component: personalDetailsForm

--- force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
+++ force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
@@ -44,7 +44,7 @@ export default class PersonalDetailsForm extends LightningElement {
     }
 
     validateEmail(email) {
-        return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     }
 
     showToast(title, message, variant) {
