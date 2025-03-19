49b44ed382e30599727287d024928ea9c04abc7e
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-03-20 08:08:40 +1100

    Update LWC component: personalDetailsForm

--- force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
+++ force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
@@ -5,6 +5,80 @@ export default class PersonalDetailsForm extends LightningElement {
     @track formData = {};
     @track errors = {};
 
+    titleOptions = [
+        { label: 'Mr', value: 'Mr' },
+        { label: 'Mrs', value: 'Mrs' },
+        { label: 'Ms', value: 'Ms' },
+        { label: 'Dr', value: 'Dr' }
+    ];
+
+    genderOptions = [
+        { label: 'Male', value: 'Male' },
+        { label: 'Female', value: 'Female' },
+        { label: 'Other', value: 'Other' }
+    ];
+
+    stateOptions = [
+        { label: 'New South Wales', value: 'NSW' },
+        { label: 'Victoria', value: 'VIC' },
+        { label: 'Queensland', value: 'QLD' },
+        { label: 'Western Australia', value: 'WA' },
+        { label: 'South Australia', value: 'SA' },
+        { label: 'Tasmania', value: 'TAS' },
+        { label: 'Australian Capital Territory', value: 'ACT' },
+        { label: 'Northern Territory', value: 'NT' }
+    ];
+
+    countryOptions = [
+        { label: 'Australia', value: 'Australia' },
+        { label: 'New Zealand', value: 'New Zealand' }
+    ];
+
+    handleInputChange(event) {
+        const { name, value } = event.target;
+        this.formData[name] = value;
+        this.validateField(name, value);
+    }
+
+    validateField(name, value) {
+        this.errors[name] = '';
+
+        switch (name) {
+            case 'firstName':
+            case 'lastName':
+            case 'addressLine1':
+            case 'suburb':
+                if (!value.trim()) {
+                    this.errors[name] = `${name} is required`;
+                }
+                break;
+            case 'postcode':
+                if (!/^\d{4}$/.test(value)) {
+                    this.errors[name] = 'Postcode must be 4 digits';
+                }
+                break;
+            case 'startDate':
+            case 'endDate':
+                if (!value) {
+                    this.errors[name] = `${name} is required`;
+                } else if (name === 'endDate' && this.formData.startDate && new Date(value) <= new Date(this.formData.startDate)) {
+                    this.errors[name] = 'End Date must be after Start Date';
+                }
+                break;
+        }
+    }
+
+    validateForm() {
+        let isValid = true;
+        Object.keys(this.formData).forEach(field => {
+            this.validateField(field, this.formData[field]);
+            if (this.errors[field]) {
+                isValid = false;
+            }
+        });
+        return isValid;
+    }
+
     handleSubmit(event) {
         event.preventDefault();
         if (this.validateForm()) {
@@ -18,64 +92,5 @@ export default class PersonalDetailsForm extends LightningElement {
         this.template.querySelector('form').reset();
         this.formData = {};
         this.errors = {};
-    }
-
-    validateField(event) {
-        const field = event.target;
-        const fieldName = field.id;
-        const fieldValue = field.value;
-
-        this.formData[fieldName] = fieldValue;
-
-        switch(fieldName) {
-            case 'firstName':
-            case 'lastName':
-                this.errors[fieldName] = fieldValue ? '' : 'This field is required.';
-                break;
-            case 'email':
-                this.errors[fieldName] = this.validateEmail(fieldValue) ? '' : 'Please enter a valid email address.';
-                break;
-            case 'phone':
-                this.errors[fieldName] = this.validatePhone(fieldValue) ? '' : 'Please enter a valid phone number (XXX) XXX-XXXX.';
-                break;
-            case 'dob':
-                this.errors[fieldName] = this.validateDate(fieldValue) ? '' : 'Please enter a valid date.';
-                break;
-            case 'zipCode':
-                this.errors[fieldName] = this.validateZipCode(fieldValue) ? '' : 'Please enter a valid zip code.';
-                break;
-            default:
-                break;
-        }
-
-        field.setCustomValidity(this.errors[fieldName]);
-        field.reportValidity();
-    }
-
-    validateForm() {
-        let isValid = true;
-        this.template.querySelectorAll('input, select').forEach(field => {
-            if (field.required && !field.value) {
-                this.errors[field.id] = 'This field is required.';
-                isValid = false;
-            }
-        });
-        return isValid && Object.values(this.errors).every(error => error === '');
-    }
-
-    validateEmail(email) {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    }
-
-    validatePhone(phone) {
-        return /^\(\d{3}\)\s\d{3}-\d{4}$/.test(phone);
-    }
-
-    validateDate(date) {
-        return !isNaN(Date.parse(date));
-    }
-
-    validateZipCode(zipCode) {
-        return /^\d{5}(-\d{4})?$/.test(zipCode);
     }
 }
