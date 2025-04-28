9d053f1a31a077a557607ca0d230ce63eaf2a18a
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-04-28 17:20:35 +1000

    Update LWC component: personalDetailsForm

--- force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
+++ force-app/main/default/lwc/personalDetailsForm/personalDetailsForm.js
@@ -1,13 +1,17 @@
 // personalDetailsForm.js
 import { LightningElement, track } from 'lwc';
-export default class PersonalDetailsForm extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class PersonalDetailsForm extends NavigationMixin(LightningElement) {
     @track formData = {};
     @track errorMessage = '';
+
     handleInputChange(event) {
         const { id, value } = event.target;
         this.formData[id] = value;
         this.validateField(id, value);
     }
+
     validateField(fieldName, value) {
         switch (fieldName) {
             case 'firstName':
@@ -32,6 +36,7 @@ export default class PersonalDetailsForm extends LightningElement {
                 break;
         }
     }
+
     isOver18(birthdate) {
         const today = new Date();
         const birthDate = new Date(birthdate);
@@ -42,48 +47,65 @@ export default class PersonalDetailsForm extends LightningElement {
         }
         return age >= 18;
     }
+
     validateDateRange() {
         const startDate = new Date(this.formData.startDate);
         const endDate = new Date(this.formData.endDate);
-        if (startDate && endDate && startDate >= endDate) {
-            this.setFieldError('endDate', 'End Date must be later than Start Date');
+        if (startDate > endDate) {
+            this.setFieldError('endDate', 'End Date must be after Start Date');
         } else {
             this.clearFieldError('endDate');
         }
     }
+
     setFieldError(fieldName, message) {
-        const field = this.template.querySelector(`[id="${fieldName}"]`);
+        const field = this.template.querySelector(`#${fieldName}`);
         field.setCustomValidity(message);
         field.reportValidity();
     }
+
     clearFieldError(fieldName) {
-        const field = this.template.querySelector(`[id="${fieldName}"]`);
+        const field = this.template.querySelector(`#${fieldName}`);
         field.setCustomValidity('');
         field.reportValidity();
     }
+
     handleSubmit() {
         if (this.validateForm()) {
             console.log('Form submitted:', this.formData);
             this.errorMessage = '';
+            this.handleClear();
         } else {
             this.errorMessage = 'Please correct the errors in the form.';
         }
     }
+
     validateForm() {
-        const allValid = [...this.template.querySelectorAll('input, select')]
+        const allValid = [...this.template.querySelectorAll('input')]
             .reduce((validSoFar, inputField) => {
                 inputField.reportValidity();
                 return validSoFar && inputField.checkValidity();
             }, true);
         return allValid;
     }
-    handleReset() {
+
+    handleClear() {
         this.formData = {};
         this.errorMessage = '';
-        this.template.querySelectorAll('input, select').forEach(field => {
-            field.value = '';
-            field.setCustomValidity('');
-            field.reportValidity();
+        this.template.querySelectorAll('input').forEach(input => {
+            input.value = '';
+            input.setCustomValidity('');
+            input.reportValidity();
+        });
+        this.template.querySelector('select').value = '';
+    }
+
+    handleCancel() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: '/'
+            }
         });
     }
 }
