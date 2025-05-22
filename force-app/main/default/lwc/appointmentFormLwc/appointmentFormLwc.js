cd6d5def9cb0b88a996bd0af60db3dcf6047eb1c
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-05-22 20:06:42 +1000

    Update LWC component: appointmentFormLwc

--- force-app/main/default/lwc/appointmentFormLwc/appointmentFormLwc.js
+++ force-app/main/default/lwc/appointmentFormLwc/appointmentFormLwc.js
@@ -2,85 +2,23 @@
 import { LightningElement, track } from 'lwc';
 
 export default class AppointmentFormLwc extends LightningElement {
-    @track formData = {
-        firstName: '',
-        middleName: '',
-        lastName: '',
-        address: '',
-        city: '',
-        state: '',
-        zipCode: ''
-    };
+    @track currentSection = null;
 
-    @track errors = {};
-    @track isLoading = false;
-
-    stateOptions = [
-        { label: 'New South Wales', value: 'NSW' },
-        { label: 'Queensland', value: 'QLD' },
-        { label: 'South Australia', value: 'SA' },
-        { label: 'Tasmania', value: 'TAS' },
-        { label: 'Victoria', value: 'VIC' },
-        { label: 'Western Australia', value: 'WA' },
-        { label: 'Australian Capital Territory', value: 'ACT' },
-        { label: 'Northern Territory', value: 'NT' }
-    ];
-
-    handleInputChange(event) {
-        const { name, value } = event.target;
-        this.formData[name] = value;
-        this.errors[name] = '';
+    connectedCallback() {
+        // Initialize the first section
+        this.currentSection = 'residential-viability';
     }
 
-    validateForm() {
-        const errors = {};
-
-        if (!this.formData.firstName) {
-            errors.firstName = 'First Name is required.';
-        }
-
-        if (!this.formData.lastName) {
-            errors.lastName = 'Last Name is required.';
-        }
-
-        if (!this.formData.zipCode) {
-            errors.zipCode = 'Zip Code is required.';
-        } else if (!/^\d{4}$/.test(this.formData.zipCode)) {
-            errors.zipCode = 'Please enter a valid 4-digit Australian postcode.';
-        }
-
-        this.errors = errors;
-        return Object.keys(errors).length === 0;
+    handleNavigationClick(event) {
+        const selectedSection = event.target.value;
+        this.currentSection = selectedSection;
     }
 
-    handleSubmit() {
-        this.isLoading = true;
-
-        if (this.validateForm()) {
-            // Submit form data to server or perform other actions
-            console.log('Form data:', this.formData);
-
-            // Simulating a delay for demonstration purposes
-            setTimeout(() => {
-                this.isLoading = false;
-                // Reset form or show success message
-            }, 2000);
-        } else {
-            this.isLoading = false;
-        }
+    handleSave() {
+        // Save logic here
     }
 
-    handleCancel() {
-        // Reset form or navigate away
-        this.formData = {
-            firstName: '',
-            middleName: '',
-            lastName: '',
-            address: '',
-            city: '',
-            state: '',
-            zipCode: ''
-        };
-        this.errors = {};
+    handleDownload() {
+        // Download logic here
     }
 }
