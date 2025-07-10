fc34729914626bcf5814994150ff41f6cdaf5b23
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-07-10 19:09:57 +1000

    Update LWC component and Apex classes: into

---
+++ force-app/main/default/lwc/into/into.js
@@ -0,0 +1,51 @@
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class QfrFormTest extends LightningElement {
+    currentPage = 1;
+    totalPages = 1;
+
+    @track formData = {
+        firstName: '',
+        lastName: ''
+    };
+
+    get currentPageString() {
+        return this.currentPage.toString();
+    }
+
+    handleInputChange(event) {
+        const field = event.target.dataset.field;
+        this.formData[field] = event.target.value;
+    }
+
+    validateForm() {
+        let isValid = true;
+        const inputs = [...this.template.querySelectorAll('lightning-input')];
+        
+        inputs.forEach(input => {
+            if (input.required && !input.value) {
+                input.reportValidity();
+                isValid = false;
+            }
+        });
+
+        return isValid;
+    }
+
+    handleSubmit() {
+        if (this.validateForm()) {
+            console.log('Form submitted:', this.formData);
+            this.showToast('Success', 'Form submitted successfully', 'success');
+        }
+    }
+
+    showToast(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
+    }
+}
