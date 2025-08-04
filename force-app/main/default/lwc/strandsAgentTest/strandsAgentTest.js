7420d435fa9106d096891383645ffc3a0e2d2fb9
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-08-04 22:30:58 +1000

    Add strandsAgentTest LWC component

--- force-app/main/default/lwc/strandsAgentTest/strandsAgentTest.js
+++ force-app/main/default/lwc/strandsAgentTest/strandsAgentTest.js
@@ -1,14 +1,76 @@
 import { LightningElement, track } from 'lwc';
 
 export default class StrandsAgentTest extends LightningElement {
+    @track isEditing = false;
     @track contactName = 'Mr test name sep name';
     @track position = 'Manager';
     @track phone = '0456456654';
     @track email = 'test30sep@health.gov.au';
 
+    // Store original values for cancel operation
+    originalValues = {
+        contactName: '',
+        position: '',
+        phone: '',
+        email: ''
+    };
+
     handleEdit() {
-        // Handle edit functionality
-        // This would typically open a modal or navigate to an edit page
-        console.log('Edit button clicked');
+        // Store original values before editing
+        this.originalValues = {
+            contactName: this.contactName,
+            position: this.position,
+            phone: this.phone,
+            email: this.email
+        };
+        this.isEditing = true;
+    }
+
+    handleCancel() {
+        // Restore original values
+        this.contactName = this.originalValues.contactName;
+        this.position = this.originalValues.position;
+        this.phone = this.originalValues.phone;
+        this.email = this.originalValues.email;
+        this.isEditing = false;
+    }
+
+    handleSave() {
+        // Validate fields
+        if (this.validateFields()) {
+            // Here you would typically make an apex call to save the data
+            // For now, we'll just exit edit mode
+            this.isEditing = false;
+        }
+    }
+
+    validateFields() {
+        const inputs = this.template.querySelectorAll('lightning-input');
+        let isValid = true;
+
+        inputs.forEach(input => {
+            if (!input.checkValidity()) {
+                input.reportValidity();
+                isValid = false;
+            }
+        });
+
+        return isValid;
+    }
+
+    handleNameChange(event) {
+        this.contactName = event.target.value;
+    }
+
+    handlePositionChange(event) {
+        this.position = event.target.value;
+    }
+
+    handlePhoneChange(event) {
+        this.phone = event.target.value;
+    }
+
+    handleEmailChange(event) {
+        this.email = event.target.value;
     }
 }
