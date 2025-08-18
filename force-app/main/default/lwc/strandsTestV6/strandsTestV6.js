ae89ac884ac870dddc3c39892158e6a349ff6193
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-08-18 22:28:48 +1000

    Update strandsTestV6 LWC JavaScript file

--- force-app/main/default/lwc/strandsTestV6/strandsTestV6.js
+++ force-app/main/default/lwc/strandsTestV6/strandsTestV6.js
@@ -1,25 +1,15 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class StrandsTestV6 extends LightningElement {
-    @track inputName = '';
-    @track displayMessage = '';
-    @track messageForChild = '';
-
-    handleNameChange(event) {
-        this.inputName = event.target.value;
-        this.messageForChild = `Hello ${this.inputName}!`;
-    }
-
     handleClick() {
-        if (this.inputName) {
-            this.displayMessage = `Welcome, ${this.inputName}!`;
-        } else {
-            this.displayMessage = 'Please enter a name.';
-        }
-    }
-
-    handleChildEvent(event) {
-        const childMessage = event.detail;
-        this.displayMessage = `Child says: ${childMessage}`;
+        // Handle button click
+        console.log('Button clicked!');
+        this.dispatchEvent(
+            new CustomEvent('buttonclick', {
+                detail: {
+                    message: 'Button was clicked'
+                }
+            })
+        );
     }
 }
