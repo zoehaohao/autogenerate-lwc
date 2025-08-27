2faad64d807982fb73865a423df0215726529570
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-08-27 17:52:19 +1000

    Add strandsTestV5 LWC component

--- force-app/main/default/lwc/strandsTestV5/strandsTestV5.js
+++ force-app/main/default/lwc/strandsTestV5/strandsTestV5.js
@@ -1,18 +1,37 @@
 import { LightningElement, api, track } from 'lwc';
 
 export default class StrandsTestV5 extends LightningElement {
-    @track inputValue = '';
-    @track displayMessage = '';
+    @track abn = '45 004 189 708';
+    @track entityName = 'COLES SUPERMARKETS AUSTRALIA PTY LTD';
+    @track activeFromDate = '14 Feb 2000';
+    @track entityType = 'Australian Private Company';
+    @track gstStatus = 'Registered';
+    @track gstDate = '01 Jul 2000';
+    @track businessLocation = 'VIC 3123';
 
-    handleInputChange(event) {
-        this.inputValue = event.target.value;
+    handleChangeField() {
+        // Handle change field button click
+        // This would typically open a modal or navigate to a new page
+        this.dispatchEvent(new CustomEvent('changefield', {
+            bubbles: true,
+            composed: true,
+            detail: {
+                currentAbn: this.abn
+            }
+        }));
     }
 
-    handleSubmit() {
-        if (this.inputValue) {
-            this.displayMessage = `Submitted value: ${this.inputValue}`;
-        } else {
-            this.displayMessage = 'Please enter a value';
+    // Method to update ABN details
+    @api
+    updateAbnDetails(abnDetails) {
+        if (abnDetails) {
+            this.abn = abnDetails.abn || this.abn;
+            this.entityName = abnDetails.entityName || this.entityName;
+            this.activeFromDate = abnDetails.activeFromDate || this.activeFromDate;
+            this.entityType = abnDetails.entityType || this.entityType;
+            this.gstStatus = abnDetails.gstStatus || this.gstStatus;
+            this.gstDate = abnDetails.gstDate || this.gstDate;
+            this.businessLocation = abnDetails.businessLocation || this.businessLocation;
         }
     }
 }
