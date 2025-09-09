cd0aef4308ed9010165b53e7b7a22b771a144cdf
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-10 09:17:34 +1000

    Add strandstestlwcv0 Lightning Web Component

--- force-app/main/default/lwc/strandstestlwcv0/strandstestlwcv0.js
+++ force-app/main/default/lwc/strandstestlwcv0/strandstestlwcv0.js
@@ -1,38 +1,10 @@
-import { LightningElement, api, track } from 'lwc';
-import processData from '@salesforce/apex/StrandstestlwcV0Controller.processData';
+import { LightningElement, api } from 'lwc';
 
 export default class Strandstestlwcv0 extends LightningElement {
-    @track inputText = '';
-    @track result;
-    @track error;
-
-    handleInputChange(event) {
-        this.inputText = event.target.value;
-    }
-
-    handleClick() {
-        if (!this.inputText) {
-            return;
-        }
-
-        processData({ inputText: this.inputText })
-            .then(result => {
-                this.result = result;
-                this.error = undefined;
-                this.dispatchEvent(new CustomEvent('success', {
-                    detail: {
-                        message: 'Data processed successfully'
-                    }
-                }));
-            })
-            .catch(error => {
-                this.error = error;
-                this.result = undefined;
-                this.dispatchEvent(new CustomEvent('error', {
-                    detail: {
-                        message: error.body.message
-                    }
-                }));
-            });
+    @api recordId;
+    
+    connectedCallback() {
+        // Component initialization logic
+        console.log('Component initialized');
     }
 }
