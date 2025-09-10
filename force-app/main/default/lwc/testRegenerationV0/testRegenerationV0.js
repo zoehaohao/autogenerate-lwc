8739052c87cd18d2d3da98bf7c85bf3a5f6063a4
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-10 16:27:53 +1000

    Update button label from 'Process' to 'Search'

--- force-app/main/default/lwc/testRegenerationV0/testRegenerationV0.js
+++ force-app/main/default/lwc/testRegenerationV0/testRegenerationV0.js
@@ -1,23 +1,7 @@
-import { LightningElement, track } from 'lwc';
-import search from '@salesforce/apex/testRegenerationV0Controller.search';
+import { LightningElement } from 'lwc';
 
 export default class TestRegenerationV0 extends LightningElement {
-    @track searchTerm = '';
-    @track results;
-    
-    handleSearchTermChange(event) {
-        this.searchTerm = event.target.value;
-    }
-    
-    handleSearch() {
-        if (this.searchTerm) {
-            search({ searchTerm: this.searchTerm })
-                .then(result => {
-                    this.results = result;
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        }
+    handleClick() {
+        console.log('Button clicked');
     }
 }
