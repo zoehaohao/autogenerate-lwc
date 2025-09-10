27b1233288f116224e0fc1b29807449f718c4300
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-10 12:58:02 +1000

    Add strandstestlwcv0 Lightning Web Component

--- force-app/main/default/lwc/strandstestlwcv0/strandstestlwcv0.js
+++ force-app/main/default/lwc/strandstestlwcv0/strandstestlwcv0.js
@@ -1,52 +1,24 @@
 import { LightningElement, api, track } from 'lwc';
-import processData from '@salesforce/apex/strandstestlwcv0Controller.processData';
 
 export default class Strandstestlwcv0 extends LightningElement {
+    @track inputValue = '';
+    
     @api recordId;
-    @track inputText = '';
-    @track processedResult = '';
-    @track error = '';
-
+    
     handleInputChange(event) {
-        this.inputText = event.target.value;
-        this.error = ''; // Clear any previous errors
+        this.inputValue = event.target.value;
+        this.dispatchEvent(new CustomEvent('valuechange', {
+            detail: {
+                value: this.inputValue
+            }
+        }));
     }
-
+    
     handleClick() {
-        if (!this.inputText) {
-            this.error = 'Please enter some text to process';
-            return;
-        }
-
-        processData({ formData: this.inputText })
-            .then(result => {
-                this.processedResult = result;
-                this.error = '';
-                
-                // Dispatch success event
-                this.dispatchEvent(new CustomEvent('success', {
-                    detail: {
-                        message: 'Data processed successfully'
-                    }
-                }));
-            })
-            .catch(error => {
-                this.error = error.body.message;
-                this.processedResult = '';
-                
-                // Dispatch error event
-                this.dispatchEvent(new CustomEvent('error', {
-                    detail: {
-                        error: error.body.message
-                    }
-                }));
-            });
-    }
-
-    @api
-    refresh() {
-        this.inputText = '';
-        this.processedResult = '';
-        this.error = '';
+        this.dispatchEvent(new CustomEvent('buttonclick', {
+            detail: {
+                value: this.inputValue
+            }
+        }));
     }
 }
