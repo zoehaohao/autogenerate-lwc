7c4cf777c2acdc7efa28b15d2981ff54b2412f31
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-11 15:58:05 +1000

    Update testRegenerationV1 input label from 'Sample Input' to 'ABN Number'

--- force-app/main/default/lwc/testRegenerationV1/testRegenerationV1.js
+++ force-app/main/default/lwc/testRegenerationV1/testRegenerationV1.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class TestRegenerationV1 extends LightningElement {}
+export default class TestRegenerationV1 extends LightningElement {
+    @track inputValue = '';
+
+    handleInputChange(event) {
+        this.inputValue = event.target.value;
+    }
+
+    handleClick() {
+        alert('Button clicked! Input value: ' + this.inputValue);
+    }
+}
