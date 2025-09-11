a64d0dee9eefdda13fc58a70ba94db745f9d2140
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-11 16:06:08 +1000

    Updated testRegenerationV1 label and created testRegenerationV2 component

--- force-app/main/default/lwc/testRegenerationV2/testRegenerationV2.js
+++ force-app/main/default/lwc/testRegenerationV2/testRegenerationV2.js
@@ -1,34 +1,9 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class TestRegenerationV2 extends LightningElement {
-    @api recordId;
     @track inputValue = '';
-    @track displayText = '';
 
     handleInputChange(event) {
         this.inputValue = event.target.value;
-        // Dispatch change event for parent components
-        this.dispatchEvent(new CustomEvent('valuechange', {
-            detail: {
-                value: this.inputValue
-            }
-        }));
-    }
-
-    handleClick() {
-        this.displayText = `You entered: ${this.inputValue}`;
-        // Dispatch click event for parent components
-        this.dispatchEvent(new CustomEvent('buttonclick', {
-            detail: {
-                value: this.inputValue,
-                timestamp: new Date().toISOString()
-            }
-        }));
-    }
-
-    @api
-    resetValues() {
-        this.inputValue = '';
-        this.displayText = '';
     }
 }
