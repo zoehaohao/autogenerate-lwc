354a317354133c01a2c3bbbaee44fa60b38564a2
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-11 11:25:45 +1000

    feat: Add input handling to abnLookupTestV12 component

--- force-app/main/default/lwc/abnLookupTestV12/abnLookupTestV12.js
+++ force-app/main/default/lwc/abnLookupTestV12/abnLookupTestV12.js
@@ -1,61 +1,13 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class AbnLookupTestV12 extends LightningElement {
-    @api label = 'Search';
-    @api placeholder = 'Search...';
-    
-    @track searchTerm = '';
-    @track results = [];
-    @track isExpanded = false;
+    inputValue = ''; // Added for new input field
 
-    get comboboxClass() {
-        return `slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click ${
-            this.isExpanded ? 'slds-is-open' : ''
-        }`;
+    handleInputChange(event) {
+        this.inputValue = event.target.value;
     }
 
-    get noResults() {
-        return this.isExpanded && this.results.length === 0;
-    }
-
-    handleKeyUp(event) {
-        this.searchTerm = event.target.value;
-        if (this.searchTerm.length >= 2) {
-            // Mock results for testing
-            this.results = [
-                { id: '1', name: 'Result 1' },
-                { id: '2', name: 'Result 2' },
-                { id: '3', name: 'Result 3' }
-            ];
-            this.isExpanded = true;
-        } else {
-            this.results = [];
-            this.isExpanded = false;
-        }
-    }
-
-    handleFocus() {
-        if (this.searchTerm.length >= 2) {
-            this.isExpanded = true;
-        }
-    }
-
-    handleBlur() {
-        // Add delay to allow click events to fire on results
-        setTimeout(() => {
-            this.isExpanded = false;
-        }, 300);
-    }
-
-    handleResultClick(event) {
-        const selectedId = event.currentTarget.dataset.id;
-        const selectedResult = this.results.find(result => result.id === selectedId);
-        if (selectedResult) {
-            this.searchTerm = selectedResult.name;
-            this.isExpanded = false;
-            this.dispatchEvent(new CustomEvent('select', {
-                detail: selectedResult
-            }));
-        }
+    handleSelectionChange(event) {
+        console.log('Selected record: ', event.detail);
     }
 }
