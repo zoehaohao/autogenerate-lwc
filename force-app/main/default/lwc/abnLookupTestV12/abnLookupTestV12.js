d04d6028cf7771984e5907255798b3c2fb72ff78
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-11 11:32:36 +1000

    Update JavaScript file for abnLookupTestV12 component

--- force-app/main/default/lwc/abnLookupTestV12/abnLookupTestV12.js
+++ force-app/main/default/lwc/abnLookupTestV12/abnLookupTestV12.js
@@ -1,13 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class AbnLookupTestV12 extends LightningElement {
-    inputValue = ''; // Added for new input field
-
-    handleInputChange(event) {
-        this.inputValue = event.target.value;
-    }
-
-    handleSelectionChange(event) {
-        console.log('Selected record: ', event.detail);
-    }
+    
 }
