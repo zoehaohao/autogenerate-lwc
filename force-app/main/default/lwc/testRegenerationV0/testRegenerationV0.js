2a7ac438547c4f126afbaebf394d0532b83f270c
zoehaohao <153477825+zoehaohao@users.noreply.github.com>
2025-09-11 12:56:00 +1000

    Create testRegenerationV0 LWC with Search button

--- force-app/main/default/lwc/testRegenerationV0/testRegenerationV0.js
+++ force-app/main/default/lwc/testRegenerationV0/testRegenerationV0.js
@@ -1,7 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import processData from '@salesforce/apex/testRegenerationV0Controller.processData';
 
 export default class TestRegenerationV0 extends LightningElement {
-    handleClick() {
-        console.log('Button clicked');
+    @track inputValue = '';
+    @track result;
+    @track error;
+
+    handleInputChange(event) {
+        this.inputValue = event.target.value;
+    }
+
+    handleSearch() {
+        if (!this.inputValue) {
+            return;
+        }
+
+        processData({ input: this.inputValue })
+            .then(result => {
+                this.result = result;
+                this.error = undefined;
+                this.dispatchEvent(new CustomEvent('success', {
+                    detail: {
+                        message: 'Operation completed successfully'
+                    }
+                }));
+            })
+            .catch(error => {
+                this.error = error.message;
+                this.result = undefined;
+                this.dispatchEvent(new CustomEvent('error', {
+                    detail: {
+                        message: error.message
+                    }
+                }));
+            });
     }
 }
